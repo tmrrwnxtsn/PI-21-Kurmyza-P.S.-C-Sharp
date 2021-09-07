c89a9946a69007c73e3c87314629bc2f70d3b9b7
tmrrwnxtsn <tmrrwnxtsn@gmail.com>
2021-09-07 12:12:37 +0400

    Rename files to more meaningful ones

--- ContainerShipApp/FormContainerShip.Designer.cs
+++ ContainerShipApp/FormContainerShip.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace ContainerShipApp
 {
-    partial class Form1
+    partial class FormContainerShip
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -32,7 +32,7 @@ namespace ContainerShipApp
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Text = "FormContainerShip";
         }
 
         #endregion
